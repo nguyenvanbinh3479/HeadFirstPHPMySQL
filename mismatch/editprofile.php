b9309093bbf54b7cf7369d4ced2639d22627ee7d
binhnguyen <nguyenvanbinh3479@gmail.com>
2019-01-02 10:40:42 +0700

    modify mismatch

--- mismatch/editprofile.php
+++ mismatch/editprofile.php
@@ -40,9 +40,7 @@
     if (!empty($new_picture)) {
       if ((($new_picture_type == 'image/gif') || ($new_picture_type == 'image/jpeg') || ($new_picture_type == 'image/pjpeg') ||
         ($new_picture_type == 'image/png')) && ($new_picture_size > 0) && ($new_picture_size <= MM_MAXFILESIZE) &&
-        ($new_picture_width <= MM_MAXIMGWIDTH) && ($new_picture_height <= MM_MAXIMGHEIGHT)) {
-        if ($_FILES['new_picture']['error'] == 0) {
-          // Move the file to the target upload folder
+        ($new_picture_width <= MM_MAXIMGWIDTH) && ($new_picture_height <= MM_MAXIMGHEIGHT)) {        if ($_FILES['file']['error'] == 0) {          // Move the file to the target upload folder
           $target = MM_UPLOADPATH . basename($new_picture);
           if (move_uploaded_file($_FILES['new_picture']['tmp_name'], $target)) {
             // The new picture file move was successful, now make sure any old picture is deleted
@@ -56,15 +54,11 @@
             $error = true;
             echo '<p class="error">Sorry, there was a problem uploading your picture.</p>';
           }
-        }
-      }
-      else {
+        }      }      else {
         // The new picture file is not valid, so delete the temporary file and set the error flag
         @unlink($_FILES['new_picture']['tmp_name']);
-        $error = true;
-        echo '<p class="error">Your picture must be a GIF, JPEG, or PNG image file no greater than ' . (MM_MAXFILESIZE / 1024) .
-          ' KB and ' . MM_MAXIMGWIDTH . 'x' . MM_MAXIMGHEIGHT . ' pixels in size.</p>';
-      }
+        $error = true;        echo '<p class="error">Your picture must be a GIF, JPEG, or PNG image file no greater than ' . (MM_MAXFILESIZE / 1024) .
+          ' KB and ' . MM_MAXIMGWIDTH . 'x' . MM_MAXIMGHEIGHT . ' pixels in size.</p>';      }
     }
 
     // Update the profile data in the database
